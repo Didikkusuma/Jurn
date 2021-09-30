c411d5a469eda782c662d37289454612970b67e3
Didikkusuma <91330192+Didikkusuma@users.noreply.github.com>
2021-09-30 20:56:01 +0700

    Add files via upload

---
+++ Latjava.java
@@ -0,0 +1,17 @@
+public class Latjava {
+
+	public static void main(String[] args){
+	
+		String Nama = "Didik Kusuma Rahmat";
+		String NIM = "6706213070";
+		String Kelas = "D3RPLA 45-05";
+		double IPK = 4.0;
+	
+		System.out.println("Nama   :"+Nama);
+		System.out.println("NIM    :"+NIM);
+		System.out.println("Kelas  :"+Kelas);
+		System.out.println("IPK    :"+IPK);
+	}
+		
+		
+}
