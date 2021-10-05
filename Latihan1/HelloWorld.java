02045d87586d077c6ab4b6a2db869452cc94c74d
Didikkusuma <didikkr30@gmail.com>
2021-10-05 13:14:46 +0700

    HelloWorld.java

---
+++ Latihan1/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama: DIDIK KUSUMA RAHMAT");
+		System.out.println("NIM: 6706213070");
+		System.out.println("Kelas: D3RPLA 45-05");
+	}
+}
+ 
